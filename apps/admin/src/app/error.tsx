7e0d665f0bad68a233fad957a668512899d943c9
Ketan Mehta <meetketanmehta@gmail.com>
2023-11-11 02:25:03 +0530

    global error and not found added in (admin)

---
+++ apps/admin/src/app/error.tsx
@@ -0,0 +1,6 @@
+"use client";
+export default function ErrorPage(){
+    return <h1 style={{ display:"flex",justifyContent:"center" ,fontWeight:'normal'}}>
+        Error : please check after some time.
+    </h1>
+}
---
+++ apps/admin/src/app/not-found.tsx
@@ -0,0 +1,6 @@
+"use client";
+export default function notFound(){
+    return <h1 style={{ display:"flex",justifyContent:"center" ,fontWeight:'normal'}}>
+        Error : Url not found.
+    </h1>
+}
