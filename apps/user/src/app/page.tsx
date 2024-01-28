88b2950fc1454e95adf82f939a0297b944a78e6b
ketan mehta <meetketanmehta@gmail.com>
2024-01-28 23:41:10 +0530

    Location is not defined solved using router.push in useEffect

--- apps/user/src/app/page.tsx
+++ apps/user/src/app/page.tsx
@@ -1,9 +1,13 @@
 'use client';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
   const router = useRouter();
-  router.push('/home');
+  useEffect(() => {
+    router.push('/home');
+  }, []);
+  // router.push('/home');
   return <h1 style={{ display: 'flex', justifyContent: 'center' }}>USER LOADED</h1>;
 }
